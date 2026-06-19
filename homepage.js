9e1f9b519b9ab36ee5ec5d54e709c12ca4aab16a
naman18vk <shivamwomp@gmail.com>
2026-06-19 12:16:35 +0530

    Refactor STATIC_PRODUCTS and related functions

--- homepage.js
+++ homepage.js
@@ -30,7 +30,7 @@ function displayItem(itemsList) {
     itemsList.forEach(product => {
         productGrid.innerHTML += `
         <div class="product-card">
-            <div class="card img"><img src="${product.image}" alt="${product.name}" onerror="this.src='./image/hero books.png'"></div>
+            <div class="card img"><img src="${product.image}" alt="${product.name}" onerror="this.src='./image/bestfriend.jpg'"></div>
             <h3>${product.name}</h3>
             <p class="price">Rs. ${product.price}</p>
             <button class="add-to-cart-btn" onclick="addToCart('${product.id}')">Add to Cart</button>
